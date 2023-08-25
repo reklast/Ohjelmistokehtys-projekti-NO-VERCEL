f6407053d21872bc18087705bd9fb14f16d3bd48
Selenius Pauli <s2300305@BC-C02FV0CDQ6LTs-MacBook-Air.local>
2023-08-25 10:20:58 +0300

    lisätty tausta login sivuun

---
+++ main.js
@@ -0,0 +1,8 @@
+// Get a reference to the button element
+let button = document.getElementById("login");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    // Change the location to the desired page URL
+    window.location.href = "/mainpage.html";
+});
