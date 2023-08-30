7fa91a6650829cf37cd02ef08f4c6031fac6590a
Nikita <reklast51@gmail.com>
2023-08-30 14:08:27 +0300

    readme changed, dashboard added to protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware';
 
-export const  config = { matcher: ['/dashboard/user', '/admin', '/']}
+export const  config = { matcher: ['/dashboard', '/user', '/admin', '/']}
