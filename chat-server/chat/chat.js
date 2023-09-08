155668f1d2d7f815ab8f709cf31210514214c62b
Marionum <105773495+marionum@users.noreply.github.com>
2023-09-08 13:05:18 +0300

    chat changes
    still trying to create chat that might work

--- chat-server/chat/chat.js
+++ chat-server/chat/chat.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = 3001;
+const port = 3000;
 const messages = [];
 
 const server = http.createServer((req, res) => {
