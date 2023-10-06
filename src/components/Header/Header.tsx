433aa63c5952cc25d762c24af11cb28e2e361140
Pauli Selenius <99865089+Mera212@users.noreply.github.com>
2023-10-06 09:34:33 +0300

    More trial and error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
 'use client'
 import React, { ReactElement } from 'react';
 import { signOut, useSession } from 'next-auth/react';
+import { MongoClient } from 'mongodb'
 
+import DbGet from '@/components/DbGet';
 import UserMenu from '@/components/UserMenu/UserMenu';
+import db from '@/lib/db';
+import getAll from '@/app/api/Request/GetAll';
 
 import styles from './Header.module.scss';
 import Link from 'next/link';
@@ -11,6 +15,9 @@ function Header(): ReactElement {
   const session  = useSession();
 
   console.log(session);
+  
+
+  
 
   return (
     <header className={styles.header}>
